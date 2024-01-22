4ea0a0801584280e9397606b0ce1a83ee70099c2
Akshay garg <gargakshay034@gmail.com>
2024-01-22 18:01:00 +0530

    Create 31.py

---
+++ 31.py
@@ -0,0 +1,10 @@
+# ***************************  Sets in Python  ***************************
+s = {1, 2, "Akshay", 6, 6, 6, 7}  # doesn't maintain order
+print(s)
+
+s1 =  {}  # gives dictionary, for set use : s1 = set()
+# s1 = set()
+print(type(s1))
+
+for i in s:
+    print(i)
