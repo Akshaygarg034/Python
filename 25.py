241f7cc9c78956fb9ede58242bc1dfe61e046269
Akshay garg <gargakshay034@gmail.com>
2024-01-22 16:40:40 +0530

    Create 25.py

---
+++ 25.py
@@ -0,0 +1,8 @@
+# ***************************  Operations on Tuples   ***************************
+
+tuple1 = (1, 2, 3, 6, 8, 45, 70, 3)
+
+print(tuple1.count(3))
+print(tuple1.index(3))
+print(tuple1.index(3, 4, 8))  # index(value, start, end) start and end are not compulsary
+print(len(tuple1))
