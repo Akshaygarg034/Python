2afa90e638f67d167ba5ea675af396db195efb7a
Akshay garg <gargakshay034@gmail.com>
2024-01-22 16:13:53 +0530

    Update 22.py

--- 22.py
+++ 22.py
@@ -6,6 +6,12 @@ print(ls[3])
 print(ls[-3])   # === ls[5 - 3] === ls[2]
 print(ls)
 
+if 3.0 in ls:
+    print("Yes")
+
+if "Ak" in "Akshay":  # Same for strings
+    print("Yes")
+
 # List Comprehension
 
 ls2 = [i for i in range(1, 11) if i%2 == 0]
