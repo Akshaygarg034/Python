157ad84333b754f9bb6c643218beb2fef1051bf7
Akshay garg <gargakshay034@gmail.com>
2024-01-22 17:25:07 +0530

    Create 29.py

---
+++ 29.py
@@ -0,0 +1,8 @@
+# ***************************  Docstrings in Python   ***************************
+
+def square(num):
+    """this is a docstring used for documenting the function"""
+    print(num**2)
+
+square(2)
+print(square.__doc__)
