bc0a36ee40cd80e98dca91057d816fca3a89143e
Akshay garg <gargakshay034@gmail.com>
2024-01-22 16:32:55 +0530

    Create 24.py

---
+++ 24.py
@@ -0,0 +1,9 @@
+# ***************************  Tuples in Python   ***************************
+
+tup = (1, 2, 3.0, "Akshay", True)
+tup2 = (1)     # It will take it as int. To convert to tuple : tup2 = (1,)
+
+print(tup, type(tup))
+print(tup2, type(tup2))
+
+print(1 in tup)
