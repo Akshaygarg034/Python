4619f8ae4135693a3601080699f2ce027dd05417
Akshay garg <gargakshay034@gmail.com>
2024-02-01 22:58:42 +0530

    Create 44.py

---
+++ 44.py
@@ -0,0 +1,8 @@
+# ***************************  How import works in Python  ***************************
+import math as m
+
+from pandas import *   # Import all the functions from pandas module but this method is not recomended
+
+print(m.sqrt(25))
+
+print(dir(m))   # To see all the functions in math module
