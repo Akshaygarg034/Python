2a04fc0b414343076d73a6b25578019f9cb7630b
Akshay garg <gargakshay034@gmail.com>
2024-02-04 23:36:01 +0530

    Create 56.py

---
+++ 56.py
@@ -0,0 +1,5 @@
+# **************************  Introduction to OOPs in Python  **************************
+
+# Procedural Programming: It is a function based programming generally preffered in action oriented tasks. If we have a scenario where we have to perform a set of actions then we can use procedural programming.
+
+# Object Oriented Programming: It is a data based programming generally preffered in state oriented tasks. If we have a scenario where we have to maintain a state then we can use object oriented programming.
