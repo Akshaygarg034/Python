75903dc675c3b712089b90a4c389f7fd8ffc769d
Akshay garg <gargakshay034@gmail.com>
2024-02-04 20:18:15 +0530

    Update 12.py

--- 12.py
+++ 12.py
@@ -8,3 +8,4 @@ print(3, str[:4])
 print(4, str[:])
 print(5, str[1:6:2]) #str[1st index, last index, step size]
 print(6, str[-4:-1]) # same as str[6-4: 6-1]
+print(7, str[::-1]) # reverse the string
