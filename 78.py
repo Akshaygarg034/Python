724fda9e5d336d465a4e3cef4ad59bd605d025b1
Akshay garg <gargakshay034@gmail.com>
2024-02-07 00:22:08 +0530

    Create 78.py

---
+++ 78.py
@@ -0,0 +1,18 @@
+# ************************  Single Inheritance in Python  ************************
+
+# Inheritance is the concept in OOPs in which one class inherits the attributes and methods of another class.
+
+# Types of Inheritance:
+
+##### 1. Single Inheritance : When a class inherits only one class.
+
+# e.g.
+ 
+class A:
+    def feature1(self):
+        print("Feature 1 is working")
+
+class B(A):
+    def feature2(self):
+        print("Feature 2 is working")
+
